52578ae1bba95808d608d856ffcb586787a590c2
Jake Walder <jakerzw@hotmail.co.uk>
2017-03-22 22:57:41 +0000

    Cursor and background working

--- Main.cpp
+++ Main.cpp
@@ -3,17 +3,20 @@
 /* prototypes */
 int init();
 void deinit();
+int loadBackground();
 
 int main()
 {  /* start of main */
 	int retval = 0;
+
 	if (init() == 0)
 	{
 		while (!key[KEY_ESC])
 		{
-			/*  put your code here  */
-			putpixel(screen, mouse_x, mouse_y, makecol(0, 255, 0));
-			clear_to_color(screen, makecol(0, 0, 0));
+			/*  main code  */
+			//putpixel(screen, mouse_x, mouse_y, makecol(0, 255, 0));
+			set_mouse_sprite(load_bitmap("sprites\\crosshair.bmp", 0));
+			show_mouse(screen);
 		}
 
 	}
@@ -29,7 +32,7 @@ END_OF_MAIN()     /* line must be included at the end of the nor
 
 
 				  /* function definitions */
-	int init()
+int init()
 {
 	int depth = 0;
 
@@ -45,19 +48,16 @@ END_OF_MAIN()     /* line must be included at the end of the nor
 	}
 	set_color_depth(depth);
 	bool graphtestOK = true;
-	int res = set_gfx_mode(GFX_AUTODETECT_WINDOWED, 1200, 640, 0, 0);
+	int res = set_gfx_mode(GFX_AUTODETECT_FULLSCREEN, 1280, 720, 0, 0);
 	if (res != 0)
 	{
-		if (set_gfx_mode(GFX_AUTODETECT_WINDOWED, 320, 200, 0, 0) != 0)
+		if (set_gfx_mode(GFX_SAFE, 320, 200, 0, 0) != 0)
 		{
-			if (set_gfx_mode(GFX_SAFE, 320, 200, 0, 0) != 0)
-			{
-				set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
-				allegro_message("Unable to set any graphic mode\n%s\n", allegro_error);
-				allegro_message(allegro_error);
-				graphtestOK = false;
-				retvalue = -1;
-			}
+			set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
+			allegro_message("Unable to set any graphic mode\n%s\n", allegro_error);
+			allegro_message(allegro_error);
+			graphtestOK = false;
+			retvalue = -1;
 		}
 	}
 	if (graphtestOK == true)
@@ -68,8 +68,8 @@ END_OF_MAIN()     /* line must be included at the end of the nor
 		retvalue = 0;
 	}
 	/* add other initializations here */
-	int install_mouse();
 
+	loadBackground();
 	return retvalue;
 }
 
@@ -78,4 +78,17 @@ void deinit()
 {
 	clear_keybuf();
 	/* add other deinitializations here */
+}
+
+int loadBackground()
+{
+	BITMAP *background = nullptr;
+	background = load_bitmap("sprites\\background.bmp", 0);
+	if (!background)
+	{
+		allegro_message("Unable to find background image\n%s\n", allegro_error);
+		return -1;
+		destroy_bitmap(background);
+	}
+	stretch_blit(background, screen, 0, 0, background->w, background->h, 0, 0, SCREEN_W, SCREEN_H);
 }
